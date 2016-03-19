077d29c126b08874944f24f487e502587f7e8415
Nick Niemeir <nick.niemeir@gmail.com>
2016-03-19 00:19:14 -0700

    Display inline scripts inside the main tag.

--- all.js
+++ all.js
@@ -1,8 +1,12 @@
-var reduce = require('universal-reduce')
-document.querySelector('main').onclick = function () {
-  console.log('test')
+function pageStart () {
+  var reduce = require('universal-reduce')
+  document.querySelector('main').onclick = function () {
+    console.log('test')
+  }
+  reduce({ foo: 'bar' }, function (acc, val, key) {
+    console.log(val, key)
+  })
+  console.log('hello world')
 }
-reduce({ foo: 'bar' }, function (acc, val, key) {
-  console.log(val, key)
-})
-console.log('hello world')
+
+window.addEventListener('load', pageStart)
